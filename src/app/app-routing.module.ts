c7db27a0e0153360185ad9892117cd8c374b4af4
brandonj17 <37629186+brandonj17@users.noreply.github.com>
2021-03-22 12:33:09 -0400

    first commit for songs-ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SongEntryComponent } from './components/song-entry/song-entry.component';
+import { SongListComponent } from './components/song-list/song-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: DashboardComponent
+  },
+  {
+    path: 'song-entry',
+    component: SongEntryComponent
+  },
+  {
+    path: 'song-list',
+    component: SongListComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SongEntryComponent } from './components/song-entry/song-entry.component';
+import { SongListComponent } from './components/song-list/song-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    DashboardComponent,
+    SongEntryComponent,
+    SongListComponent
   ],
   imports: [
     BrowserModule,
